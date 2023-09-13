ff833c2b54739c1918eaa1776b3f9740b02a7a34
zinnie1025 <luckyjj1025@gmail.com>
2023-09-13 13:02:20 +0900

    파일 추가

---
+++ Pipeline/jieun/Userprofile.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+class UserProfile:
+    '''
+    기존 User가 높은 평점 부여한 숙소 대상 으로,
+    기존 User 성향 Profile 파악
+    '''
+    '''
+    #TODO
+    1. UserID 입력
+    2. 높은 평점읇 부여한 숙소 X 리스트 추출
+    3. 2번에서 추출한 숙소 X와 유사한 숙소 Y 리스트 추출
+    '''
+    def __init__(self, df):
+        pass
+        
+        
+        
+        
+        
+ 
