92e8e836bb3d4c2ab2d4b86f4fd2417a585a191a
zinnie1025 <luckyjj1025@gmail.com>
2023-09-13 13:02:00 +0900

    코드 수정

--- Pipeline/jieun/__init__.py
+++ Pipeline/jieun/__init__.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("/Users/genie/Documents/COLLABORATION/AirbnbWise/Pipeline/user_10000db.csv")
+    df = pd.read_csv("/Users/genie/Documents/COLLABORATION/AirbnbWise/Pipeline/csv/user_10000db.csv")
     ex = pp.InputDataProcessor(df)
     output = ex.processing()
     output.drop(columns=['region'],inplace=True)
-    output.to_csv("/Users/genie/Documents/COLLABORATION/AirbnbWise/Pipeline/preprocessed_user_10000db.csv", index=False)
+    output.to_csv("/Users/genie/Documents/COLLABORATION/AirbnbWise/Pipeline/csv/preprocessed_user_10000db.csv", index=False)
