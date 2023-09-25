94d556f9d21165e585daaae5065800026e64b4bd
zinnie1025 <luckyjj1025@gmail.com>
2023-09-25 10:04:53 +0900

    클래스 코드 추가

--- Utils/KitContainer/sentiword.py
+++ Utils/KitContainer/sentiword.py
@@ -5,8 +5,17 @@ warnings.filterwarnings("ignore")
 import os
 import re
 from tqdm import tqdm
-from kiwipiepy import Kiwi
 
+from kiwipiepy import Kiwi
+from itertools import chain
+from collections import Counter
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+import plotly.express as px
 class preSentiment:
     '''
     text 준비
@@ -48,6 +57,7 @@ class sentimentKiwi:
     def spacingCorrection(self):
         '''
         텍스트 띄어쓰기 교정
+        #! 여기 왜 오래걸리지????
         '''
         space_comments = []
         with tqdm(total=len(self.filtered_comment)) as pbar:
@@ -61,7 +71,7 @@ class sentimentKiwi:
         '''
         여러 문장으로 구성된 텍스트 분리
         '''
-        split_result = self.kiwi.split_into_sentences(space_comments)
+        split_result = self.kiwi.split_into_sents(space_comments)
         split_result = list(split_result)
         return split_result        
     
@@ -75,7 +85,7 @@ class sentimentKiwi:
             for comment in split_result:
                 text.extend([sentence[0] for sentence in comment])
                 token.extend([sentence[3] for sentence in comment])
-
+                pbar.update(1)
         sentence_df = pd.DataFrame(
             {
                 'text' : text,
@@ -83,3 +93,78 @@ class sentimentKiwi:
             }
         )
         return sentence_df
+    
+    def splitNVR(self, sentence_df):
+        '''
+        명사, 동사, 어근 분리
+        nouns : 명사 -> NNG : 일반 명사, NNP : 고유 명사
+        neg_verb: 부정 동사 -> VCN : 부정 동사
+        verb : 동사 -> VA : 형용사, VV : 동사, VX : 보조 용언(시제, 높임말)
+        radix : 어근 
+        '''
+        nouns = []
+        verbs = []
+        neg_verbs = []
+        radixs = []
+
+        for morphs in sentence_df['token']:
+            nouns.append([(noun[0], noun[1]) for noun in morphs if noun[1] in ['NNG', 'NNP']])
+            neg_verbs.append([morphs for noun in morphs if noun[1] in ['VCN']])
+            verbs.append([(verb[0]+'다', verb[1]) for verb in morphs if verb[1] in ['VA', 'VV', 'VX']])
+            radixs.append([(verb[0], verb[1]) for verb in morphs if verb[1] in ['XR']])
+
+        sentence_df['verbs'] = verbs
+        sentence_df['nouns'] = nouns
+        sentence_df['radixs'] = radixs
+        return sentence_df
+    
+class preVisualizationtext:
+    '''
+    wordCloud 텍스트 준비
+    '''
+    def __init__(self, sentence_df):
+        self.sentence_df = sentence_df
+        
+    def splitWord(self, row):
+        wordList = [word[0] for words in self.sentence_df[row] for word in words]
+        return wordList
+    
+    def wordCount(self, wordList):
+        word_counts = Counter(wordList)
+        return word_counts
+
+class wordCloud:
+    '''
+    visualization
+    '''
+    def generate_word_cloud(self, data, font_path, title, color=None, save_path=None):
+        wordcloud = WordCloud(width=800, height=400,
+                            max_words=200,
+                            scale=2,
+                            random_state=42,
+                            background_color='white',
+                            colormap = color,
+                            font_path=font_path).generate_from_frequencies(data)
+        if save_path:
+            wordcloud.to_file(save_path)
+
+class TreeMapGenerator:
+    '''
+    TreeMap visualization
+    '''
+    def TreeMapGenerator(self, labels, values, title, save_treemapPath=None):
+        fig = go.Figure(go.Treemap(
+            labels=labels[:30],
+            parents=[''] * len(labels[:30]),
+            values=values,
+            texttemplate="%{label}<br>%{value}",
+            hoverinfo='label+value',
+        ))
+
+        fig.update_layout(
+            title=title,
+            margin=dict(l=10, r=10, t=40, b=10)
+        )
+
+        if save_treemapPath:
+            fig.write_image(save_treemapPath)
